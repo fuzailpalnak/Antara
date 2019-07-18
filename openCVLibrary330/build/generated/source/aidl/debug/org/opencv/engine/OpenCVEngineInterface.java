dec7097027052baef3e8f9d4487b537ecc5c4f4b
Fuzail palnak <fuzailpalnak@gmail.com>
2019-07-18 16:25:11 +0530

    Folder restructure

--- openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/palnak/PersonalProjects/AndroidStudioProjects/antara/Antara/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/palnak/PersonalProjects/AndroidStudioProjects/Antara/Antara/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
